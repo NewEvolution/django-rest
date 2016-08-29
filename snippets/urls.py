c707bf9c29f6697b4a8a93a8c7eb00afe238bfa7
Ryan Tanay <ryantanay@newevolution.org>
2016-08-29 15:37:54 -0500

    Plurals, my old nemesis.

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippets/$', view.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', view.snippet_detail),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
