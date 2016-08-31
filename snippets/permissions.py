31a48957d2534a0b7d4e4c61ca525d76ea44bcc3
Ryan Tanay <ryantanay@newevolution.org>
2016-08-31 13:16:35 -0500

    Too much JavaScript lately...

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -10,7 +10,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Read permissions are allowed for any reuqest,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
-            return true
+            return True
 
         # Write permissions are only allowed to the owner of the snippet.
         return obj.owner == request.user
