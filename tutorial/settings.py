b35588d3879d2f52a7003478eaba8083f7db2ca3
Ryan Tanay <ryantanay@newevolution.org>
2016-08-31 15:20:58 -0500

    Added REST Framework settings section to settings.py and set pagination

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST Famework
+# http://www.django-rest-framework.org/api-guide/settings/
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
