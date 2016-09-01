ec2cbffe646beb471e349733c7e5e8223d8454cf
Ryan Tanay <ryantanay@newevolution.org>
2016-09-01 14:22:15 -0500

    Modified URLs to use new views

--- snippets/urls.py
+++ snippets/urls.py
@@ -25,22 +25,12 @@ user_detail = UserViewSet.as_view({
 
 # API Endpoints
 urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail'),
-    url(r'^snippets/$',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
+    url(r'^$', api_root),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^snippets/$', snippet_list, name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
 ])
 
 # Login and logout views for the browsable API
