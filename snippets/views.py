f413665b7018d66828602199b26a773e77f7cc37
Ryan Tanay <ryantanay@newevolution.org>
2016-09-01 14:12:02 -0500

    removed extraneous imports & reorganized imports

--- snippets/views.py
+++ snippets/views.py
@@ -1,8 +1,10 @@
 from snippets.models import Snippet
-from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
+
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions, renderers, viewsets
+
+from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
